1470da63e7df611b44572a157f7eacb5b228366e
Sufyan Shaik <mdsufyan7@gmail.com>
2024-10-19 16:50:07 +0530

    first class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import About from './comp/About'
+import Contact from './comp/Contact'
 
-function App() {
+export default function App() {
+
+  let x = 5
+  x = 10
+  let y = 5
+  let z = x + y
+
+  function run() {
+    window.alert('hai')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
 
-export default App;
+      <h2>Main</h2>
+      <h1>{x} and {y} = {z}</h1>
+      <button onClick={run}>Click here</button>
+
+      <About />
+      <Contact />
+
+    </div>
+  )
+}
